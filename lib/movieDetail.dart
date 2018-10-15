dbbd2158a3c05b2d44f21003ce94d80c67ff5de7
GurukathirJ <gkathir15@gmail.com>
2018-10-15 19:14:20 +0530

    launching link in chrome

--- lib/movieDetail.dart
+++ lib/movieDetail.dart
@@ -6,14 +6,23 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'model/MoviesResponse.dart';
 import 'package:share/share.dart';
-//import 'package:flutter_share/flutter_share.dart';
-
+import 'package:flutter_share/flutter_share.dart';
+import 'package:advanced_share/advanced_share.dart' show AdvancedShare;
+import 'package:url_launcher/url_launcher.dart';
 
 final String id = "";
 
 class MovieDetail extends StatelessWidget {
   final Movies movies;
   MovieDetail({Key key, @required this.movies}) : super(key: key);
+
+  Future<Null> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +35,22 @@ class MovieDetail extends StatelessWidget {
         child: new Column(
           children: <Widget>[
             new Image.network(movies.backgroundImage),
-            new Text(movies.summary),
+            new Text(
+              movies.summary,
+            ),
             new InkWell(
               onTap: () {
-                Share.share(movies.torrents[0].url);
+                _launchInBrowser(movies.torrents[0].url);
+                // Share.share("data:text/link;" + movies.torrents[0].url);
+
+                // AdvancedShare.generic(movies.title,movies.torrents[0].url,Link to Torrent,)
               },
-              child: new Text("Torrent" + movies.titleEnglish),
+              child: new Text(
+                "Torrent" + movies.titleEnglish,
+                style: new TextStyle(
+                  fontSize: 15.0,
+                ),
+              ),
             )
           ],
         ),
